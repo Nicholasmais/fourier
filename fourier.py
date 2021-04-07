7a3b84287250694d0cfb769ecfe9f902425d4821
Nícholas Eras <48137930+Nicholamais@users.noreply.github.com>
2021-04-07 16:03:03 -0300

    v1.0

--- fourier.py
+++ fourier.py
@@ -4,12 +4,12 @@ from math import *
 x = [i for i in arange(-30,31,0.1)]
 y = [0.0]*len(x)
 
-fun = input("Digite a expressão (n para somatorio dever ser maiusculo N!\n")
+fun = input("Digite a expressão (n para somatorio dever ser maiusculo (N))!\n")
 fun1=fun.replace("N","(i+1)")
 fun2=fun1.replace('x','*x[val]')
 
 for val in range(0,len(x)):
-    for i in range (0,2):
+    for i in range (0,3):
         y[val] += eval(fun2)
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
--- fouriertk.py
+++ fouriertk.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-from math import sin
+from math import *
 from numpy import arange
 
 janela = tk.Tk()
@@ -33,9 +33,12 @@ def conta():
     y = [0.0] * len(x)
     ini = int(text.get())
     fim = int(text2.get())
+    fun = func.get()
+    fun1 = fun.replace("N", "(i+1)")
+    fun2 = fun1.replace('x', '*x[val]')
     for val in range(0, len(x)):
-        for i in range(ini, fim + 1):
-            y[val] += sin((i) * x[val]) / (i)
+        for i in range(ini-1, fim ):
+            y[val] += eval(fun2)
     ax.clear()
     ax.spines['left'].set_position(('data', 0.0))
     ax.spines['bottom'].set_position(('data', 0.0))
@@ -48,12 +51,11 @@ def conta():
 
 frame_esquerda = tk.Frame(janela)
 frame_esquerda.pack(side='left', fill='both', expand=True)
-frame_direita = tk.Frame(janela)
-frame_direita.pack(side='right', fill='both', expand=True)
+
 
 somato = tk.LabelFrame(frame_esquerda, relief='ridge', bd=5, width=30, height=5, text="Somatório")
 frame_esquerda.update()
-somato.place(x=frame_esquerda.winfo_width() * 0.45, y=20)
+somato.place(x=frame_esquerda.winfo_width() * 0.22, y=20)
 
 text = tk.Entry(somato)
 text.grid(row=1, column=1)
@@ -69,6 +71,15 @@ btn2.grid(row=3, column=2)
 
 btn.config(command=conta)
 btn2.config(command=conta)
+
+funclab = tk.LabelFrame(frame_esquerda, relief='ridge', bd=5, height=5, text="Função")
+funclab.place(x=frame_esquerda.winfo_width() * 0.375, y=20)
+
+func = tk.Entry(funclab, width=100)
+func.pack()
+
+grap = tk.Button(funclab, width=10,text="Plotar", command=conta)
+grap.pack()
 
 janela.minsize(janelaHeight, janelaWidth)
 janela.mainloop()
